da271707a6d780b47ca01a6afcc7e91702531d0d
slamach <drslampro@gmail.com>
2020-02-09 21:11:14 +0300

    Subclasses getters added

--- src/main/java/data/Chapter.java
+++ src/main/java/data/Chapter.java
@@ -3,4 +3,12 @@ package data;
 public class Chapter {
     private String name; //Поле не может быть null, Строка не может быть пустой
     private long marinesCount; //Значение поля должно быть больше 0, Максимальное значение поля: 1000
+
+    public String getName() {
+        return name;
+    }
+
+    public long getMarinesCount() {
+        return marinesCount;
+    }
 }
--- src/main/java/data/Coordinates.java
+++ src/main/java/data/Coordinates.java
@@ -3,4 +3,12 @@ package data;
 public class Coordinates {
     private double x;
     private Float y; //Максимальное значение поля: 262, Поле не может быть null
+
+    public double getX() {
+        return x;
+    }
+
+    public Float getY() {
+        return y;
+    }
 }
