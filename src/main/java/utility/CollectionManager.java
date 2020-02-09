187f8a4245a39328b8a01417120ae783b797fbc0
slamach <drslampro@gmail.com>
2020-02-10 01:57:11 +0300

    Commands moved to CommandManager

--- src/main/java/utility/CollectionManager.java
+++ src/main/java/utility/CollectionManager.java
@@ -20,23 +20,4 @@ public class CollectionManager {
         collection.add(new SpaceMarine("Test2", new Coordinates(36.0, 41.0F), 56.0, AstartesCategory.ASSAULT,
                        Weapon.BOLT_PISTOL, MeleeWeapon.POWER_FIST, new Chapter("TestChapter2", 398L)));
     }
-
-    public void help() {
-        System.out.println("Список команд:");
-        System.out.printf("%-35s%-1s%n"," info","вывести информацию о коллекции");
-        System.out.printf("%-35s%-1s%n"," show","вывести все элементы коллекции");
-        System.out.printf("%-35s%-1s%n"," add {element}","добавить новый элемент в коллекцию");
-        System.out.printf("%-35s%-1s%n"," update <ID> {element}","обновить значение элемента коллекции по ID");
-        System.out.printf("%-35s%-1s%n"," remove_by_id <ID>","удалить элемент из коллекции по ID");
-        System.out.printf("%-35s%-1s%n"," clear","очистить коллекцию");
-        System.out.printf("%-35s%-1s%n"," save","сохранить коллекцию в файл");
-        System.out.printf("%-35s%-1s%n"," execute_script <file_name>","исполнить скрипт из указанного файла");
-        System.out.printf("%-35s%-1s%n"," exit","завершить программу (без сохранения в файл)");
-        System.out.printf("%-35s%-1s%n"," add_if_min {element}","добавить новый элемент, если его значение меньше, чем у наименьшего");
-        System.out.printf("%-35s%-1s%n"," remove_greater {element}","удалить из коллекции все элементы, превышающие заданный");
-        System.out.printf("%-35s%-1s%n"," history","вывести последние 8 команд");
-        System.out.printf("%-35s%-1s%n"," sum_of_health","вывести сумму значений поля health для всех элементов коллекции");
-        System.out.printf("%-35s%-1s%n"," max_by_melee_weapon","вывести элемент, значение поля meleeWeapon которого максимально");
-        System.out.printf("%-35s%-1s%n"," filter_by_weapon_type <weaponType>","вывести элементы, значение поля weaponType которых равно заданному");
-    }
 }
--- src/main/java/utility/CommandManager.java
+++ src/main/java/utility/CommandManager.java
@@ -6,7 +6,7 @@ public class CommandManager {
     private final int commandHistoryAmount = 8;
 
     private CollectionManager collectionManager;
-    private String[] userCommands = new String[commandHistoryAmount];
+    // private String[] userCommands = new String[commandHistoryAmount];
 
     public CommandManager(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
@@ -24,7 +24,7 @@ public class CommandManager {
 
                 switch (userCommand) {
                     case "help":
-                        collectionManager.help();
+                        help();
                         break;
                     case "exit":
                         break;
@@ -33,5 +33,24 @@ public class CommandManager {
                 }
             }
         }
+    }
+
+    public void help() {
+        System.out.println("Список команд:");
+        System.out.printf("%-35s%-1s%n"," info","вывести информацию о коллекции");
+        System.out.printf("%-35s%-1s%n"," show","вывести все элементы коллекции");
+        System.out.printf("%-35s%-1s%n"," add {element}","добавить новый элемент в коллекцию");
+        System.out.printf("%-35s%-1s%n"," update <ID> {element}","обновить значение элемента коллекции по ID");
+        System.out.printf("%-35s%-1s%n"," remove_by_id <ID>","удалить элемент из коллекции по ID");
+        System.out.printf("%-35s%-1s%n"," clear","очистить коллекцию");
+        System.out.printf("%-35s%-1s%n"," save","сохранить коллекцию в файл");
+        System.out.printf("%-35s%-1s%n"," execute_script <file_name>","исполнить скрипт из указанного файла");
+        System.out.printf("%-35s%-1s%n"," exit","завершить программу (без сохранения в файл)");
+        System.out.printf("%-35s%-1s%n"," add_if_min {element}","добавить новый элемент, если его значение меньше, чем у наименьшего");
+        System.out.printf("%-35s%-1s%n"," remove_greater {element}","удалить из коллекции все элементы, превышающие заданный");
+        System.out.printf("%-35s%-1s%n"," history","вывести последние 8 команд");
+        System.out.printf("%-35s%-1s%n"," sum_of_health","вывести сумму значений поля health для всех элементов коллекции");
+        System.out.printf("%-35s%-1s%n"," max_by_melee_weapon","вывести элемент, значение поля meleeWeapon которого максимально");
+        System.out.printf("%-35s%-1s%n"," filter_by_weapon_type <weaponType>","вывести элементы, значение поля weaponType которых равно заданному");
     }
 }
