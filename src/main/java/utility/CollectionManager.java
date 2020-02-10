2469dcc62a3cb3c047bba922d53df29567255c23
slamach <drslampro@gmail.com>
2020-02-10 13:08:22 +0300

    info() function + ui fixes

--- src/main/java/utility/CollectionManager.java
+++ src/main/java/utility/CollectionManager.java
@@ -1,14 +1,45 @@
 package utility;
 
 import java.util.TreeSet;
+import java.time.LocalDateTime;
 
 import data.*;
 
 public class CollectionManager {
     private TreeSet<SpaceMarine> collection = new TreeSet<SpaceMarine>();
+    private LocalDateTime lastInitTime = null;
+    private LocalDateTime lastSaveTime = null;
+
 
     public CollectionManager() {
         load();
+    }
+
+    public int getSize() {
+        return collection.size();
+    }
+
+    public String getCollectionType() {
+        return collection.getClass().getName();
+    }
+
+    public int getCollectionSize() {
+        return collection.size();
+    }
+
+    public String getLastInitTime() {
+        if (lastInitTime == null) return "в данной сессии инициализации еще не происходило";
+        return lastInitTime.toLocalDate().toString() + " " + lastInitTime.toLocalTime().toString();
+    }
+
+    public String getLastSaveTime() {
+        if (lastSaveTime == null) return "в данной сессии сохранения еще не происходило";
+        return lastSaveTime.toLocalDate().toString() + " " + lastSaveTime.toLocalTime().toString();
+    }
+
+    public void save() {
+        // TODO: Сохранение коллекции в файл
+        lastSaveTime = LocalDateTime.now();
     }
 
     private void load() {
@@ -19,5 +50,7 @@ public class CollectionManager {
         
         collection.add(new SpaceMarine("Test2", new Coordinates(36.0, 41.0F), 56.0, AstartesCategory.ASSAULT,
                        Weapon.BOLT_PISTOL, MeleeWeapon.POWER_FIST, new Chapter("TestChapter2", 398L)));
+        
+        lastInitTime = LocalDateTime.now();
     }
 }
--- src/main/java/utility/CommandManager.java
+++ src/main/java/utility/CommandManager.java
@@ -18,13 +18,15 @@ public class CommandManager {
         try (Scanner commandScanner = new Scanner(System.in)) {
             while (!userCommand.equals("exit")) {
                 // TODO: Обработка ошибок
-                System.out.print(">>> ");
+                System.out.print("\n>>> ");
                 userCommand = commandScanner.nextLine().trim();
-                System.out.println();
 
                 switch (userCommand) {
                     case "help":
                         help();
+                        break;
+                    case "info":
+                        info();
                         break;
                     case "exit":
                         break;
@@ -37,20 +39,28 @@ public class CommandManager {
 
     public void help() {
         System.out.println("Список команд:");
-        System.out.printf("%-35s%-1s%n"," info","вывести информацию о коллекции");
-        System.out.printf("%-35s%-1s%n"," show","вывести все элементы коллекции");
-        System.out.printf("%-35s%-1s%n"," add {element}","добавить новый элемент в коллекцию");
-        System.out.printf("%-35s%-1s%n"," update <ID> {element}","обновить значение элемента коллекции по ID");
-        System.out.printf("%-35s%-1s%n"," remove_by_id <ID>","удалить элемент из коллекции по ID");
-        System.out.printf("%-35s%-1s%n"," clear","очистить коллекцию");
-        System.out.printf("%-35s%-1s%n"," save","сохранить коллекцию в файл");
-        System.out.printf("%-35s%-1s%n"," execute_script <file_name>","исполнить скрипт из указанного файла");
-        System.out.printf("%-35s%-1s%n"," exit","завершить программу (без сохранения в файл)");
-        System.out.printf("%-35s%-1s%n"," add_if_min {element}","добавить новый элемент, если его значение меньше, чем у наименьшего");
-        System.out.printf("%-35s%-1s%n"," remove_greater {element}","удалить из коллекции все элементы, превышающие заданный");
-        System.out.printf("%-35s%-1s%n"," history","вывести последние 8 команд");
-        System.out.printf("%-35s%-1s%n"," sum_of_health","вывести сумму значений поля health для всех элементов коллекции");
-        System.out.printf("%-35s%-1s%n"," max_by_melee_weapon","вывести элемент, значение поля meleeWeapon которого максимально");
-        System.out.printf("%-35s%-1s%n"," filter_by_weapon_type <weaponType>","вывести элементы, значение поля weaponType которых равно заданному");
+        System.out.printf("%-37s%-1s%n"," info","вывести информацию о коллекции");
+        System.out.printf("%-37s%-1s%n"," show","вывести все элементы коллекции");
+        System.out.printf("%-37s%-1s%n"," add {element}","добавить новый элемент в коллекцию");
+        System.out.printf("%-37s%-1s%n"," update <ID> {element}","обновить значение элемента коллекции по ID");
+        System.out.printf("%-37s%-1s%n"," remove_by_id <ID>","удалить элемент из коллекции по ID");
+        System.out.printf("%-37s%-1s%n"," clear","очистить коллекцию");
+        System.out.printf("%-37s%-1s%n"," save","сохранить коллекцию в файл");
+        System.out.printf("%-37s%-1s%n"," execute_script <file_name>","исполнить скрипт из указанного файла");
+        System.out.printf("%-37s%-1s%n"," exit","завершить программу (без сохранения в файл)");
+        System.out.printf("%-37s%-1s%n"," add_if_min {element}","добавить новый элемент, если его значение меньше, чем у наименьшего");
+        System.out.printf("%-37s%-1s%n"," remove_greater {element}","удалить из коллекции все элементы, превышающие заданный");
+        System.out.printf("%-37s%-1s%n"," history","вывести последние 8 команд");
+        System.out.printf("%-37s%-1s%n"," sum_of_health","вывести сумму значений поля health для всех элементов коллекции");
+        System.out.printf("%-37s%-1s%n"," max_by_melee_weapon","вывести элемент, значение поля meleeWeapon которого максимально");
+        System.out.printf("%-37s%-1s%n"," filter_by_weapon_type <weaponType>","вывести элементы, значение поля weaponType которых равно заданному");
+    }
+
+    public void info() {
+        System.out.println("Сведения о коллекции:");
+        System.out.println(" Тип: " + collectionManager.getCollectionType());
+        System.out.println(" Количество элементов: " + collectionManager.getCollectionSize());
+        System.out.println(" Дата последнего сохранения: " + collectionManager.getLastSaveTime());
+        System.out.println(" Дата последней инициализации: " + collectionManager.getLastInitTime());
     }
 }
