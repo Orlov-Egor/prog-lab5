7d8337019fa6d7c3a69aaf47cefcab587af58492
slamach <drslampro@gmail.com>
2020-02-20 01:00:18 +0300

    Finished save-load. Fixes.

--- src/main/java/utility/FileManager.java
+++ src/main/java/utility/FileManager.java
@@ -12,37 +12,42 @@ import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonParseException;
 
 import data.SpaceMarine;
 
 public class FileManager {
-    private File marinesCollectionFromFile = new File ("marinesCollection.json");
     private Gson gson = new Gson();
+    private File collectionFile;
+
+    public FileManager() {
+        collectionFile = new File("marinesCollection.json");
+    }
 
     public void writeCollection(Collection<?> collection) {
-    	try {
-    		FileWriter collectionFileWriter = new FileWriter(marinesCollectionFromFile);
+    	try (FileWriter collectionFileWriter = new FileWriter(collectionFile)) {
         	collectionFileWriter.write(gson.toJson(collection));
-        	collectionFileWriter.flush();
-        	collectionFileWriter.close();
-    	}catch(IOException exception){
-    		System.out.println("Ошибка записи коллекции в загрузочный файл!");
+    	} catch (IOException exception) {
+    		System.out.println("Загрузочный файл является директорией/не может быть открыт!");
     	}
     }
 
     public TreeSet<SpaceMarine> readCollection() {
-        try (Scanner collectionFileScanner = new Scanner(marinesCollectionFromFile)) {
-        	TreeSet<SpaceMarine> marinesCollection;
-        	Type collectionType = new TypeToken<TreeSet<SpaceMarine>>(){}.getType();
-            String collectionFromFile = collectionFileScanner.nextLine();
-            marinesCollection = gson.fromJson(collectionFromFile.toString(), collectionType);
-            return marinesCollection;
-        }
-        catch(NoSuchElementException exception) {
-            System.out.println("В загрузочном файле не обнаружена коллекция!");
-        }
-        catch(FileNotFoundException exception) {
+        try (Scanner collectionFileScanner = new Scanner(collectionFile)) {
+        	TreeSet<SpaceMarine> collection;
+            Type collectionType = new TypeToken<TreeSet<SpaceMarine>>(){}.getType();
+            collection = gson.fromJson(collectionFileScanner.nextLine().trim(), collectionType);
+            System.out.println("Коллекция успешна загружена!");
+            return collection;
+        } catch (NoSuchElementException exception) {
+            System.out.println("Загрузочный файл пуст!");
+        } catch (FileNotFoundException exception) {
             System.out.println("Загрузочный файл не найден!");
+        } catch (JsonParseException exception) {
+            System.out.println("В загрузочном файле не обнаружена коллекция!");
+        } catch (IllegalStateException exception) {
+            System.out.println("Непредвиденная ошибка!");
+            System.exit(0);
         }
         return new TreeSet<SpaceMarine>();
     }
